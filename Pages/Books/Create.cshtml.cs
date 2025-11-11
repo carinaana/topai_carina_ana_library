90bef34b58894cbf03cfaf47e1d843e0a9b9df88
Topai Carina Ana <topai.carina@yahoo.ro>
2025-11-11 12:04:15 +0200

    push lab6

--- Pages/Books/Create.cshtml.cs
+++ Pages/Books/Create.cshtml.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using topai_carina_ana_lab2.Data;
 using topai_carina_ana_lab2.Models;
 
 namespace topai_carina_ana_lab2.Pages.Books
 {
+    [Authorize(Roles = "Admin")]
     public class CreateModel : BookCategoriesPageModel
     {
         private readonly topai_carina_ana_lab2.Data.topai_carina_ana_lab2Context _context;
